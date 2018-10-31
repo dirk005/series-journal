7dee12cb3fc960c272af6099cc67dee13bac5cd3
Dirk <dirkvanrensburg5@gmail.com>
2018-10-31 15:56:06 +0200

    connected login to theTvDB on my server

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -6,27 +6,17 @@ class MainPage extends Component {
 	constructor(props){
 		super(props);
 		this.state ={
-			apikey : '6IRSO76GZPTPQ0U8',
-			userkey : 'EPUUU79RZ0JT31IM',
-			username: 'dirkvanrensburg5o1b'
+			
 		}	
 	}
 	componentDidMount(){
 		this.connectToApi();		
 	}
 	 connectToApi = () => {
-		fetch('https://api.thetvdb.com/login',{
-				method : 'post',
-				headers : {'Content-Type': 'application/json'},
-				body : JSON.stringify({
-					apikey : this.state.apikey,
-					userkey : this.state.userkey,
-					username : this.state.username
-				})
-			}).then(response => response.json())
-			.then(details => console.log(details))
-
-
+	 	fetch('http://localhost:3001/tvdblogin')
+	 	.then(response => response.json())
+	 	.then(res => console.log(res))
+		.catch(err => console.log(err))	
 	}	
   render() {
     
